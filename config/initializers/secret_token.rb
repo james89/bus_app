fb9f9a9271fdb3c4cbdfbb2dc42216e9e5227122
James Rothpearl <jrothpearl@gmail.com>
2013-11-20 16:41:12 -0500

    first commit for jasmine/travis testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusApp::Application.config.secret_token = 'c75d47401a6adb66d98601f38dd4d84cf09228f3a8ab1f196300e9a3fb962c4ff604e9a4ec9a78ad8cb802c4fe7dccb9195b08310a8bd87c76c9cd673b1113c0'
